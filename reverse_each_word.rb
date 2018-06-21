86d1d074d542a190d62050c04c3417873687784a
Vicki Hamm <vicki.cello@gmail.com>
2018-06-21 14:30:37 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,14 @@
-string = String.new("Hello Buddy Pants")
-yes_string = string.split( )
-
-def reverse_each_word(yes_string)
-  yes_string.each do |word|
-    puts word.reverse
-    return yes_string.reverse
+def reverse_each_word(string)
+  new_string = []
+  string.split.each do |word|
+    new_string << word.reverse
   end
+    new_string.join(" ")
 end
 
-def reverse_each_word(yes_string)
-  yes_string.map do |word|
+def reverse_each_word(string)
+  array = string.split.collect do |word|
     word.reverse
   end
+  array.join(" ")
 end
